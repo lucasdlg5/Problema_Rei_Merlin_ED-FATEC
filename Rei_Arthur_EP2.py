579c2e3750eaa24e65770f6a45413d0bfb0c7f6e
lucasdlg5 <lucasdomingosleao55@hotmail.com>
2018-09-13 16:50:24 -0300

    Init algoritm

--- Rei_Arthur_EP2.py
+++ Rei_Arthur_EP2.py
@@ -1,11 +1,20 @@
 import re
+def verifica_encalhada(lista):
+    return 1
+
 with open (".\casamento.txt","r") as casamento:
+    damas_nomes,damas_c, cavaleiros_nomes = [], [], []
     damas = casamento.readlines()
     for dama in damas:
-        #d = dama.split(" ")
-        print (re.findall(r"[\w']+",dama))
-        #print (d)
-    
+        
+        damas_c = re.findall(r"[\w']+",dama) #Guardando lista de casamento das damas com cavaleiros
+        damas_nomes.append(damas_c[0]) #Guardando nome das damas
+        for c in damas_c[1:]: #Percorrendo a lista de cavaleiros disponiveis escolhido pelas damas, ignorando a primeira casa
+            cavaleiros_nomes.append(c) #Guardando nomes dos cavaleiros
+
+        
+    print ("Nome das damas: %s" % damas_nomes)
+    print ("Nome dos cavaleiros: %s" % cavaleiros_nomes)
     casamento.close()
 
 
