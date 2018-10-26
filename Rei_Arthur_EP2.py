da7c75a43ee557aba3f60cb8a1649504aca24ed6
Daniel Willians Ignacio de Souza <daniel2wis@gmail.com>
2018-10-26 11:03:59 -0200

    Funcionando - verificar fazendo testes diferentes

--- Rei_Arthur_EP2.py
+++ Rei_Arthur_EP2.py
@@ -6,8 +6,8 @@ def verifica_encalhada(Lista_escolhas_damas):
     return 1
 
 
-with open (".\casamento.txt","r") as casamento:
-    damas_nomes,damas_c, cavaleiros_nomes, damas_escolhas, temp, index = [], [], [], [], [], 0
+with open (".\casamento2.txt","r") as casamento:
+    damas_nomes,damas_c, cavaleiros_nomes, damas_escolhas, temp, index, cont_noivas = [], [], [], [], [], 0, 0
     damas = casamento.readlines()
     for dama in damas:
         
@@ -24,7 +24,7 @@ with open (".\casamento.txt","r") as casamento:
 
     print ("\n Nome das damas: '{0}'" .format(damas_nomes))
     print ("\n Cavaleiros escolhidos pelas damas: '{0}'".format( cavaleiros_nomes))
-    print ("\n Escolha das damas com cavaleiros: '{0}'".format( damas_escolhas))
+    print ("\n Escolha das damas com cavaleiros: '{0}' \n".format( damas_escolhas))
 
 
     # Função recursiva para escolha dos candidatos
@@ -32,13 +32,22 @@ with open (".\casamento.txt","r") as casamento:
         escolha = list(escolha[0].split())  #Remover os espacos em branco para separar as palavras de -> ['Jessica Adriano'] para -> ['Jessica', 'Adriano']      
         for verifica_disponibilidade in (escolha[1:]): #O escolha[1:] Faz ignorar o primeiro dado do Array Ex.: ['Jessica', 'Adriano']  para ['Adriano'] 
             temp = []
-            temp = set(verifica_disponibilidade) #Criando a lista em modo SET para verificar os conjuntos
+            temp = set([verifica_disponibilidade]) #Criando a lista em modo SET para verificar os conjuntos OBS.: Foi obrigatorio inserir os colchetes na variavel do FOR para que a funcao do ISSUBSET funcionacsse para buscar se é um conjunto ou nao!
             if (temp.issubset(cavaleiros_nomes)): #Verificando se o nome do cavaleiro faz parte do conjunto dos cavaleiros, caso seja verdadeiro irei remover da lista de referencia atestando como disponivel e escolhido, caso contrario Verifico o proximo cavaleiro disponvel escolhido pela dama
-                print ("o nome '{0}' está na lista: \n '{1}".format(verifica_disponibilidade, cavaleiros_nomes))
-            else:
-                print ("o nome '{0}' NÃO está na lista: \n '{1}'".format(verifica_disponibilidade, cavaleiros_nomes))
-                #VERIFICAR PORQUE ESTA CAINDO TUDO NO ELSE!
-    
+                # print ("o nome '{0}' ESTÁ na lista: \n '{1}".format(verifica_disponibilidade, cavaleiros_nomes))
+                # print ("O nome '{0}' ESTÁ na lista! Removendo. ".format(verifica_disponibilidade))
+                cavaleiros_nomes.pop(cavaleiros_nomes.index(verifica_disponibilidade)) #Primeiro localizo a posicao do nome do cavaleiro no array usando cavaleiros_nomes.index("nome") para depois remover com o POP da lista
+                # print (cavaleiros_nomes)
+                cont_noivas +=1 #Faz a contagem se todas as noivas conseguiram seus pares!
+                break #Precisa desse break para sair do FOR, senão ira percorrer todos os candidatos escolhido pelas damas e remover todos eles mesmo já com um disponivel
+            # else:
+                # print ("O nome '{0}' NÃO está na lista: \n '{1}'".format(verifica_disponibilidade, cavaleiros_nomes))
+                # print ("A noiva '{0}' ficou sem par! Pois o '{1}' não estava disponivel.".format(escolha[:1],verifica_disponibilidade))
+                
+    if (cont_noivas != len(damas_escolhas)): #Atraves da contagem de quantas noivas sairam com pares, preciso confirmar se este numero condiz com a lista inicial!
+        print ("A quantidade de escolhas '{0}' foi diferente de numero de noivas '{1}'".format (cont_noivas, len(damas_escolhas)))
+    else:
+        print("Todas as noivas tiveram seus pares!!")
     casamento.close()
 
 
